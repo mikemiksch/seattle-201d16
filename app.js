bcf30c8b9fd6bee5baa639dba7ac5361ea964447
Ashley Ross <ashley.e.ross@gmail.com>
2016-10-25 08:20:42 -0700

    updating w style changes

--- app.js
+++ app.js
@@ -6,25 +6,30 @@ var Pike = {
   maxCust: 65,
   avgCookieSales: 6.3,
   cookiesPerHour: [],
+  calcCookiesPerHour: [],
 
   predictedCustomers: function() {
     return Math.Floor(Math.random() * (this.maxCust - this.minCust) + this.minCust);
-  },
-  calcCookiesPerHour: function(){
-    for (var i = 0; i < hours.length ; i++){
+    console.log('hello');
+  }
+
+  calcCookiesPerHour: function() {
+    for (var i = 0; i < hours.length ; i++) {
       var cookies = Math.Floor(this.avgCookieSales * this.predictedCustomers());
       this.cookiesPerHour.push(cookies);
     }
-  }
-};
+   }
+  };
 calcCookiesPerHour();
 
-// function createHtml(){
-//   var h2 = document.createElement('h2');
-//   h2.innerText = 'Pike';
-//   var main = document.getElementsByTagName('main')[0];
-//   main.appendChild(h2);
-//   var ul = document.createElement('ul');
-//   ;
-// }
-// createHtml();
+function createHtml(){
+  var h2 = document.createElement('h2');
+  h2.innerText = 'Pike';
+  var main = document.getElementsByTagName('main')[0];
+  main.appendChild(h2);
+  var ul = document.createElement('ul');
+  var hourlySales = calcCookiesPerHour();
+  var li = document.createElement(hourlySales[i]);
+  ;
+}
+createHtml();
