04242b0340dfcc812ecccbf2931839bce5fb589c
Ashley Ross <ashley.e.ross@gmail.com>
2016-10-25 12:07:35 -0700

    first commit of new files in tuesday-tables

--- app.js
+++ app.js
@@ -1,28 +1,52 @@
 'use strict';
-var hours = ['6am' ,'6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm', '8pm'];
+
+// located here as a global value that can be used for all locations
+var hours = ['6am' ,'6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm'];
 
 var Pike = {
+  name: 'first-and-pike',
   minCust: 23,
   maxCust: 65,
   avgCookieSales: 6.3,
   cookiesPerHour: [],
-  calcCookiesPerHour: [],
-
+// calculate and store the avg number of customers per hour
   predictedCustomers: function() {
     return Math.Floor(Math.random() * (this.maxCust - this.minCust) + this.minCust);
     console.log('hello');
+    // randomNumCust: function()
   }
-
+// calculate and store the avg number of cookie sales per hour
   calcCookiesPerHour: function() {
     for (var i = 0; i < hours.length ; i++) {
       var cookies = Math.Floor(this.avgCookieSales * this.predictedCustomers());
-      this.cookiesPerHour.push(cookies);
+      this.cookiesPerHour.push(Math.floor(this.predictedCustomers() * this.cookies));
     }
-   }
-  };
-calcCookiesPerHour();
+  }
+};
+
+
+renderHTMLSales: fucnction(){
+  this.calcCookiesPerHour():
+  var h2 = document.createElement('h2')
+  h2.innerText = this.name;
+  var ul = document.createElement('ul');
+  main.appendChild(ul);
+  for (var i = 0; i < hours.length; i++)
+  var li = document.createElement('li');
+  li.innerText = hours[i] + ': ' + this.calcCookiesPerHour[i] + 'cookies';
+  ul.appendChild(li);
+
+}
+
+for var = 0; i < dailySales.lenth; i++){
+  totalSales += this.dailySales[i];
+  ul.appendChild(li);
+}
 
 function createHtml(){
+  var h1 = document.createElement('h1');
+  h1.innerText = "All Cookie Stores";
+  main.appendChild(h1);
   var h2 = document.createElement('h2');
   h2.innerText = 'Pike';
   var main = document.getElementsByTagName('main')[0];
@@ -32,4 +56,6 @@ function createHtml(){
   var li = document.createElement(hourlySales[i]);
   ;
 }
-createHtml();
+pike.predictedCustomers();
+pike.calcCookiesPerHour();
+pike.renderHTMLSales();
