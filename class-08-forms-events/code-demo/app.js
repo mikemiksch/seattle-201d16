8aa8689d728bc2bbe9fe4d6cfd88f3e1a6c7a2fb
Nicholas Hunt-Walker <nhuntwalker@gmail.com>
2016-10-26 11:00:01 -0700

    Forms and styling them

--- class-08-forms-events/code-demo/app.js
+++ class-08-forms-events/code-demo/app.js
@@ -18,3 +18,18 @@ cars[0].drive(); // somehow, this car instance has access to the method, which d
 
 new Car(2);
 cars[1].drive(); // also has the method
+
+// AN ENTIRELY SEPARATE THING!
+// I want the total of all stores across all hours
+// And add it to the total row as the last td element
+
+var totalRow = document.getElementById('total-row');
+var totalSales = 0;
+for (var i = 1; i < totalRow.children.length; i++){
+  var td = totalRow.children[i];
+  var number = parseInt(td.innerText);
+  totalSales += number;
+}
+var td = document.createElement('td');
+td.innerText = totalSales;
+totalRow.appendChild(td);
